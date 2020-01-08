ca12bbf526992a9ac1c8466744ed42b7c2bbf492
Peng Du <rick.peng.du@gmail.com>
2020-01-07 22:25:39 -0800

    added reverse array

---
+++ ReverseArray/src/Main.java
@@ -0,0 +1,55 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("testReverseArray " + (testReverseArray() ? "PASSED" : "FAILED"));
+    }
+
+    public static void reverseArray(int[] values) {
+        int size = values.length;
+        for (int i=0; i<size/2; i++) {
+            int temp = values[i];
+            values[i] = values[size-1-i];
+            values[size-1-i] = temp;
+        }
+    }
+
+    public static boolean testReverseArray() {
+        // prepare the answers
+        int inputs[][] = {
+                {},
+                {1},
+                {-2, 2},
+                {3, -3},
+                {1, 2, 3},
+                {3, 2, 1, 0},
+                {-4, -4, -4},
+                {-1, 0, 9, 3, 2}};
+
+        int outputs[][] = {
+                {},
+                {1},
+                {2, -2},
+                {-3, 3},
+                {3, 2, 1},
+                {0, 1, 2, 3},
+                {-4, -4, -4},
+                {2, 3, 9, 0, -1}};
+
+        for (int i=0; i<inputs.length; i++) {
+
+            reverseArray(inputs[i]);
+
+            for (int j=0; j<inputs[i].length; j++) {
+
+                int expected = outputs[i][j];
+                int actual = inputs[i][j];
+
+                if (expected!=actual) {
+                    System.out.printf("Expected %d, actual %d\n", expected, actual);
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
