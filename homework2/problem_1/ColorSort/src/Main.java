e6906ada97887ad4a0d788cc8ffdebac85b257a6
Peng Du <rick.peng.du@gmail.com>
2020-01-15 20:38:33 -0800

    add color sort homework

---
+++ homework2/problem_1/ColorSort/src/Main.java
@@ -0,0 +1,20 @@
+/*
+CSS143B homework 2, problem 1
+by [your name]
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("testColorSort " + (testColorSort() ? "PASSED" : "FAILED"));
+    }
+
+    public static void colorSort(int[] data) {
+        // homework
+        return; // place holder
+    }
+
+    public static boolean testColorSort() {
+        // homework
+        return false; // place holder
+    }
+}
